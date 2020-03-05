8cc18c734bd689102c1eb924e299ed9f6807b2b5
Brendan Byrd <GitHub@ResonatorSoft.org>
2020-03-05 18:22:12 -0500

    Add some extra niceties

---
+++ Source/output-tarball.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd output/Languages/EnglishPoorlyTranslated
+rm ../../../../1.1/Languages/EnglishPoorlyTranslated\ \(English\ \(Poor\)\).tar
+tar vcf ../../../../1.1/Languages/EnglishPoorlyTranslated\ \(English\ \(Poor\)\).tar *
